001352d63532ca9ea7581c8f10bf31a94c704aff
timoniq <timonik.ch@gmail.com>
2019-10-22 18:01:14 +0300

    v0.2 setup py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="vkbottle",
+    version="1.0",
+    author="timoniq",
+    description="Description..",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/timoniq/vkbottle",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=["aiohttp", "pydantic", "colorama", "termcolor"],
+)
