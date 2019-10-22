6c8477e571677f452bc63653e42fbf616845f543
timoniq <timonik.ch@gmail.com>
2019-10-22 17:51:29 +0300

    f i x

--- vkbottle/const.py
+++ vkbottle/const.py
@@ -2,7 +2,7 @@ __version__ = '0.2'  # Package VKBottle version
 
 __author__ = 'Arseniy Timonik'
 
-API_VERSION = 5.101  # VK Api version
+API_VERSION = 5.102  # VK Api version
 
 API_URL = 'https://api.vk.com/method/'
 
