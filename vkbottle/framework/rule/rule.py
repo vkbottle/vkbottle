54f6b616f81e6a427b6a6ee90c6c953a88382dbc
Даниил <51062020+DarklyZ@users.noreply.github.com>
2020-02-25 18:41:24 +0500

    Event object choose (#39)

--- vkbottle/framework/rule/rule.py
+++ vkbottle/framework/rule/rule.py
@@ -1,5 +1,6 @@
 import typing
 import json
+import inspect
 
 from copy import copy
 from vbml import Pattern, Patcher
@@ -27,12 +28,14 @@ class AbstractRule(Copy):
     def __init_subclass__(cls, **kwargs):
         cls.call: typing.Optional[typing.Callable] = None
         cls.context: RuleExecute = RuleExecute()
+        cls.getfullargspec = None
 
     def create(self, func: typing.Callable, data: dict = None):
         self.call: typing.Callable = func
         self.context = RuleExecute()
         if data is not None:
             setattr(self, "data", {**getattr(self, "data", {}), **data})
+        self.getfullargspec = inspect.getfullargspec(self.call)
 
     async def check(self, event):
         ...
@@ -139,6 +142,8 @@ class EventRule(AbstractRule):
         self.data = {"event": event}
 
     async def check(self, event):
+        if "data" not in self.data:
+            self.data["data"] = self.getfullargspec.annotations.get(self.getfullargspec.args[0], dict)
         for e in self.data["event"]:
             if e == event:
                 return True
