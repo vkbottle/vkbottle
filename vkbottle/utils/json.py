c6dd0e6c7a6116c38ef62292da68e7ed4c0f2247
Даниил <51062020+DarklyZ@users.noreply.github.com>
2020-02-27 23:26:40 +0500

    Update json.py

--- vkbottle/utils/json.py
+++ vkbottle/utils/json.py
@@ -1,7 +1,7 @@
 import pkg_resources
 
-SUPPORTED_MODULES = ["json", "ujson", "hyperjson", "orjson"]
+SUPPORTED_MODULES = ["ujson", "hyperjson", "orjson"]
 DOWNLOADED = [pkg.key for pkg in pkg_resources.working_set if pkg.key in SUPPORTED_MODULES]
-USAGE = DOWNLOADED[-1]
+USAGE = DOWNLOADED[-1] if DOWNLOADED else "json"
 
 json = __import__(USAGE)
