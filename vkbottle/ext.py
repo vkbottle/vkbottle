e101776659efe4fde518c23d228b306415aa8bb7
timoniq <52574917+timoniq@users.noreply.github.com>
2020-03-23 12:47:41 +0300

    Revert "v2.6 concatenate_blueprint_from_dir"
    This reverts commit 44fc93e9fde5b92edcbcde331e2e499040cc5a99.

--- vkbottle/ext.py
+++ vkbottle/ext.py
@@ -1,16 +1,1 @@
 from .handler.middleware import Middleware
-from .framework import Bot
-import glob, importlib, os
-
-
-def concatenate_blueprint_from_dir(bot: Bot, dirname: str, dp: str = "dp"):
-    all_list = list()
-    for f in glob.glob(os.path.dirname(__file__) + "/*.py"):
-        if os.path.isfile(f) and not os.path.basename(f).startswith('_'):
-            all_list.append(os.path.basename(f)[:-3])
-
-    dispatcher = [importlib.import_module(dirname + "." + m, __package__) for m in all_list]
-    for module in dispatcher:
-        bot.on.concatenate(getattr(module, dp))
-
-    return bot, dispatcher
