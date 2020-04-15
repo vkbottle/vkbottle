13aad6aacca1687604925d50042453e95a5ec5a7
timoniq <52574917+timoniq@users.noreply.github.com>
2020-04-15 10:45:57 +0300

    added user_id setter

--- vkbottle/api/api/api.py
+++ vkbottle/api/api/api.py
@@ -100,6 +100,10 @@ class API(ContextInstanceMixin):
     def group_id(self, group_id: int):
         self._group_id = group_id
 
+    @user_id.setter
+    def user_id(self, user_id: int):
+        self._user_id = user_id
+
     def __dict__(self):
         return {"generator": self.token_generator.__class__.__qualname__, "throw_errors": self.throw_errors}
 
