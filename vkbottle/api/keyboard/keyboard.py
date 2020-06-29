18b224d9c41fe3ac93eb0827b72c8b35bb44cb28
yilbegan <w2v@protonmail.com>
2020-06-29 15:38:04 +0300

    Add support for older versions (#154)

--- vkbottle/api/keyboard/keyboard.py
+++ vkbottle/api/keyboard/keyboard.py
@@ -58,7 +58,7 @@ class Keyboard:
 
     def add_button(self, action: typing.Union[Text], color: str = None):
         if not len(self.buttons):
-            self.row()
+            raise KeyboardError("Firstly add row with keyboard.add_row()")
         button = KeyboardButton(action, color)
         self.buttons[-1].append(button)
         return button
