5c18c6277651500b245036eaa2bba3dc9c1afdca
dhvcc <1337kwiz@gmail.com>
2020-07-08 15:04:35 +0300

    Changed deprecated skip_defaults to exclude_unset Fixed docs/api.ru.md generator link and changed camel_case danger as the previous variant seems to be unsupported by github markdown

--- vkbottle/types/base.py
+++ vkbottle/types/base.py
@@ -8,11 +8,11 @@ class BaseModel(pydantic.BaseModel):
         use_enum_values = True
 
     def __str__(self):
-        return str(self.dict(skip_defaults=True))
+        return str(self.dict(exclude_unset=True))
 
     def __repr__(self):
         args = ", ".join(
-            [f"{key}={value}" for key, value in self.dict(skip_defaults=True).items()]
+            [f"{key}={value}" for key, value in self.dict(exclude_unset=True).items()]
         )
         return "{}({})".format(self.__class__.__name__, args)
 
