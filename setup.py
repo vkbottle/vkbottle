06d09df329b8285ebe54ac69cb7e84657b5a6252
timoniq <52574917+timoniq@users.noreply.github.com>
2020-08-21 06:48:30 +0300

    added loguru 2 dependencies

--- setup.py
+++ setup.py
@@ -27,5 +27,5 @@ setuptools.setup(
         "Programming Language :: Python :: 3.7",
         "Operating System :: OS Independent",
     ],
-    install_requires=["aiohttp", "pydantic", "contextvars", "vbml==0.5.93", "watchgod", ],
+    install_requires=["aiohttp", "pydantic", "contextvars", "vbml==0.5.93", "watchgod", "loguru"],
 )
