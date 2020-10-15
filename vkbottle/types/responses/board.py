e0fee415dcd4708f33dea1a00c795f7094c2299f
MRRollMan <konkow7@gmail.com>
2020-10-15 22:35:23 +0300

    Fixed board types

--- vkbottle/types/responses/board.py
+++ vkbottle/types/responses/board.py
@@ -6,21 +6,23 @@ from vkbottle.types import objects
 class GetTopics(BaseModel):
     count: int = None
     items: typing.List[objects.board.Topic] = None
+    default_order: int = None
     can_add_topics: objects.base.BoolInt = None
+    profiles: typing.List[objects.users.UserFull]
 
 
 class GetTopicsModel(BaseModel):
     response: GetTopics = None
 
 
-AddTopic = typing.Dict
+AddTopic = int
 
 
 class AddTopicModel(BaseModel):
     response: AddTopic = None
 
 
-CreateComment = typing.Dict
+CreateComment = int
 
 
 class CreateCommentModel(BaseModel):
