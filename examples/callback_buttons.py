b31da756a94b42836bcdc5aa88aaa4d868b5e929
Daniil Markelov <danik3399@gmail.com>
2020-11-08 01:16:20 -0600

    Added code example for Callback-buttons (#252)

---
+++ examples/callback_buttons.py
@@ -0,0 +1,34 @@
+# Example of sending and receiving an event after pressing the Callback button
+# Documentation: https://vk.cc/aC9JG2
+
+import os
+import logging
+
+from vkbottle import Keyboard, Callback, GroupTypes, GroupEventType
+from vkbottle.bot import Bot, Message
+
+bot = Bot(os.environ["TOKEN"])
+logging.basicConfig(level=logging.INFO)
+
+KEYBOARD = Keyboard(one_time=False).add(Callback("Callback-кнопка", payload={"cmd": "callback"})).get_json()
+
+
+@bot.on.private_message(text="/callback")
+async def send_callback_button(message: Message):
+    await message.answer("Лови!", keyboard=KEYBOARD)
+
+
+@bot.on.raw_event(GroupEventType.MESSAGE_EVENT, dataclass=GroupTypes.MessageEvent)
+async def handle_message_event(event: GroupTypes.MessageEvent):
+    # The event_data parameter accepts three object types
+    # In this example we will consider "show_snackbar" type
+
+    await bot.api.messages.send_message_event_answer(
+        event_id=event.object.event_id,
+        user_id=event.object.user_id,
+        peer_id=event.object.peer_id,
+        event_data='{"type":"show_snackbar", "text":"Сейчас я исчезну"}'
+    )
+
+
+bot.run_forever()
