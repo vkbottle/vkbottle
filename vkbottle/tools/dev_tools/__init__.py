516a3dc02e29cfc7ea5b4eea91ed31797736675d
Ne0shka <neosha.73@gmail.com>
2021-01-11 19:26:29 +0400

    Fix (#302)

--- vkbottle/tools/dev_tools/__init__.py
+++ vkbottle/tools/dev_tools/__init__.py
@@ -6,6 +6,5 @@ from .template import TemplateElement, template_gen
 from .loop_wrapper import DelayedTask, LoopWrapper
 from .mini_types import BotTypes, message_min
 from .storage import ABCStorage, CtxStorage
-from .template import TemplateElement, template_gen
 from .utils import convert_shorten_filter, load_blueprints_from_package, run_in_task
 from .vkscript_converter import vkscript
