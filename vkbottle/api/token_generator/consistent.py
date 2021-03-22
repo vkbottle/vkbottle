0c07140ac34963d4d62ec56ddb6c2429bc87522b
InspectorDue <70595103+InspectorDue@users.noreply.github.com>
2021-03-23 01:35:38 +0500

    Change list to itertools.cycle (#318 close #315)
    `itertools.cycle` gives you an option not to calculate the index

--- vkbottle/api/token_generator/consistent.py
+++ vkbottle/api/token_generator/consistent.py
@@ -1,3 +1,4 @@
+import itertools
 from typing import List
 
 from .abc import ABCTokenGenerator
@@ -5,10 +6,7 @@ from .abc import ABCTokenGenerator
 
 class ConsistentTokenGenerator(ABCTokenGenerator):
     def __init__(self, tokens: List[str]):
-        self.tokens = tokens
-        self.index = 0
+        self.tokens = itertools.cycle(tokens)
 
     async def get_token(self) -> str:
-        i = self.index
-        self.index = i + 1 if i + 1 < len(self.tokens) else 0
-        return self.tokens[i]
+        return next(self.tokens)
