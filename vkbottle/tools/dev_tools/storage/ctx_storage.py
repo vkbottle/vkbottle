1e69fa54a7aa9b6926bf9197ac7802ce67fa9ceb
homus32 <homus32.reset@mail.ru>
2021-04-20 19:18:19 +1100

    fix typing x2

--- vkbottle/tools/dev_tools/storage/ctx_storage.py
+++ vkbottle/tools/dev_tools/storage/ctx_storage.py
@@ -1,5 +1,5 @@
 import typing
-from typing import Optional
+from typing import Hashable
 
 from vkbottle.tools.dev_tools.ctx_tool import BaseContext
 
@@ -22,18 +22,18 @@ class CtxStorage(ABCStorage, BaseContext):
             self.storage = default
             self.set_instance(self)
 
-    def set(self, key: Optional[str, int, tuple], value: typing.Any) -> None:
+    def set(self, key: Hashable, value: typing.Any) -> None:
         current_storage = self.get_instance().storage
         current_storage[key] = value
         self.set_instance(CtxStorage(current_storage, True))
 
-    def get(self, key: Optional[str, int, tuple]) -> typing.Any:
+    def get(self, key: Hashable) -> typing.Any:
         return self.get_instance().storage.get(key)
 
-    def delete(self, key: Optional[str, int, tuple]) -> None:
+    def delete(self, key: Hashable) -> None:
         new_storage = self.get_instance().storage
         new_storage.pop(key)
         self.set_instance(CtxStorage(new_storage, True))
 
-    def contains(self, key: Optional[str, int, tuple]) -> bool:
+    def contains(self, key: Hashable) -> bool:
         return key in self.get_instance().storage
