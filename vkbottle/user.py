834e8c7217bf7cb7b6e683334b9ac7331f28a00e
dhvcc <1337kwiz@gmail.com>
2021-06-02 21:12:26 +0300

    Returned import to user.py, dynamic ignores for autoflake

--- vkbottle/user.py
+++ vkbottle/user.py
@@ -1,5 +1,7 @@
-from .framework.user import UserBlueprint
-from .tools.dev.mini_types.bot import MessageMin
+from .dispatch.rules import user as rules
+from .dispatch.views import user as views
+from .framework.user import ABCUserLabeler, User, UserBlueprint, UserLabeler, run_multibot
+from .tools.dev_tools.mini_types.bot import MessageMin
 
 Message = MessageMin
 Blueprint = UserBlueprint
