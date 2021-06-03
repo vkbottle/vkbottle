03af80deb679c950cf8b4f8ac290d2ba98fe1fdd
dhvcc <1337kwiz@gmail.com>
2021-06-03 15:29:57 +0300

    Patched code, fixed BotMessageView.get_state_key
    Removed unnecessary code, updated .dev_tools import

--- vkbottle/user.py
+++ vkbottle/user.py
@@ -1,7 +1,7 @@
 from .dispatch.rules import user as rules
 from .dispatch.views import user as views
 from .framework.user import ABCUserLabeler, User, UserBlueprint, UserLabeler, run_multibot
-from .tools.dev_tools.mini_types.bot import MessageMin
+from .tools.dev.mini_types.bot import MessageMin
 
 Message = MessageMin
 Blueprint = UserBlueprint
