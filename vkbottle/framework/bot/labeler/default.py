6e55406b3475e70723c8f2c8ec689a3b4b7c797a
FeeeeK <krmrclemens@gmail.com>
2021-06-27 16:25:48 +0300

    Added `re.DOTALL` to default vbml flags

--- vkbottle/framework/bot/labeler/default.py
+++ vkbottle/framework/bot/labeler/default.py
@@ -81,7 +81,7 @@ class BotLabeler(ABCBotLabeler):
 
         # Rule config is accessible from every single custom rule
         self.rule_config: Dict[str, Any] = {
-            "vbml_flags": re.MULTILINE,  # Flags for VBMLRule
+            "vbml_flags": re.MULTILINE | re.DOTALL,  # Flags for VBMLRule
             "vbml_patcher": vbml.Patcher(),  # Patcher for VBMLRule
         }
 
