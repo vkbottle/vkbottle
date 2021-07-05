057d89df0a1c2a35c2f142c65e4ca5a5c7cbb364
FeeeeK <krmrclemens@gmail.com>
2021-07-05 23:41:42 +0300

    Update `MessageMin`

--- vkbottle/tools/dev_tools/mini_types/bot/message.py
+++ vkbottle/tools/dev_tools/mini_types/bot/message.py
@@ -1,8 +1,18 @@
-from typing import Any, List, Optional, Union
+from typing import Any, Callable, List, Optional, Union
 
 from vkbottle_types import StatePeer
 from vkbottle_types.events.bot_events import MessageNew
-from vkbottle_types.objects import ClientInfoForBots, MessagesMessage, UsersUser
+from vkbottle_types.objects import (
+    AudioAudio,
+    ClientInfoForBots,
+    DocsDoc,
+    MessagesMessage,
+    PhotosPhoto,
+    UsersUser,
+    VideoVideo,
+    WallWallComment,
+    WallWallpostFull,
+)
 
 from vkbottle.api import ABCAPI, API
 
@@ -16,6 +26,14 @@ class MessageMin(MessagesMessage):
     @property
     def ctx_api(self) -> Union["ABCAPI", "API"]:
         return getattr(self, "unprepared_ctx_api")
+
+    @property
+    def chat_id(self) -> int:
+        return self.peer_id - 2_000_000_000
+
+    @property
+    def message_id(self) -> int:
+        return self.conversation_message_id or self.id
 
     async def get_user(self, raw_mode: bool = False, **kwargs) -> Union["UsersUser", dict]:
         raw_user = (await self.ctx_api.request("users.get", {"user_ids": self.from_id, **kwargs}))[
@@ -53,6 +71,44 @@ class MessageMin(MessagesMessage):
         data["peer_id"] = self.peer_id
 
         return (await self.ctx_api.request("messages.send", data))["response"]
+
+    def get_wall_attachment(self) -> List["WallWallpostFull"]:
+        result = [attachment.wall for attachment in self.attachments if attachment.wall]
+        return result or None
+
+    def get_wall_reply_attachment(self) -> List["WallWallComment"]:
+        result = [
+            attachment.wall_reply for attachment in self.attachments if attachment.wall_reply
+        ]
+        return result or None
+
+    def get_photo_attachments(self) -> List["PhotosPhoto"]:
+        return [attachment.photo for attachment in self.attachments if attachment.photo]
+
+    def get_video_attachments(self) -> List["VideoVideo"]:
+        return [attachment.video for attachment in self.attachments if attachment.video]
+
+    def get_doc_attachments(self) -> List["DocsDoc"]:
+        return [attachment.doc for attachment in self.attachments if attachment.doc]
+
+    def get_audio_attachments(self) -> List["AudioAudio"]:
+        return [attachment.audio for attachment in self.attachments if attachment.audio]
+
+    def get_message_id(self) -> int:
+        return self.id or self.conversation_message_id
+
+    def get_payload_json(
+        self,
+        throw_error: bool = False,
+        unpack_failure: Callable[[str], dict] = lambda payload: payload,
+        json: Any = __import__("json"),
+    ) -> Union[dict, None]:
+        try:
+            return json.loads(self.payload)
+        except (json.decoder.JSONDecodeError, TypeError) as e:
+            if throw_error:
+                raise e
+        return unpack_failure(self.payload)
 
 
 MessageMin.update_forward_refs()
