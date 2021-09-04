1b7c81e509779587318a44578bcc8c8a03554000
INFERNO256 <84619552+INFERNO256@users.noreply.github.com>
2021-09-04 21:27:09 +0500

    Add `reply` and `forward` methods to `MessageMin`

--- vkbottle/tools/dev/mini_types/bot/message.py
+++ vkbottle/tools/dev/mini_types/bot/message.py
@@ -2,7 +2,7 @@ from typing import Any, List, Optional, Union
 
 from vkbottle_types import StatePeer
 from vkbottle_types.events.bot_events import MessageNew
-from vkbottle_types.objects import MessagesClientInfo, MessagesMessage, UsersUser
+from vkbottle_types.objects import ClientInfoForBots, MessagesForward, MessagesMessage, UsersUser
 
 from vkbottle.api import ABCAPI, API
 
@@ -54,6 +54,73 @@ class MessageMin(MessagesMessage):
 
         return (await self.ctx_api.request("messages.send", data))["response"]
 
+    async def reply(
+        self,
+        message: Optional[str] = None,
+        attachment: Optional[str] = None,
+        user_id: Optional[int] = None,
+        domain: Optional[str] = None,
+        chat_id: Optional[int] = None,
+        user_ids: Optional[List[int]] = None,
+        lat: Optional[float] = None,
+        long: Optional[float] = None,
+        reply_to: Optional[int] = None,
+        forward_messages: Optional[List[int]] = None,
+        forward: Optional[str] = None,
+        sticker_id: Optional[int] = None,
+        group_id: Optional[int] = None,
+        keyboard: Optional[str] = None,
+        payload: Optional[str] = None,
+        dont_parse_links: Optional[bool] = None,
+        disable_mentions: Optional[bool] = None,
+        **kwargs,
+    ) -> int:
+        locals().update(kwargs)
+        locals().pop("kwargs")
+        data = {k: v for k, v in locals().items() if k != "self" and v is not None}
+        data["peer_id"] = self.peer_id
+        data["forward"] = MessagesForward(
+            conversation_message_ids=[self.conversation_message_id],
+            peer_id=self.peer_id,
+            is_reply=True
+        ).json()
+
+        return (await self.ctx_api.request("messages.send", data))["response"]
+
+    async def forward(
+        self,
+        message: Optional[str] = None,
+        attachment: Optional[str] = None,
+        peer_id: Optional[int] = None,
+        user_id: Optional[int] = None,
+        domain: Optional[str] = None,
+        chat_id: Optional[int] = None,
+        user_ids: Optional[List[int]] = None,
+        lat: Optional[float] = None,
+        long: Optional[float] = None,
+        reply_to: Optional[int] = None,
+        forward_messages: Optional[List[int]] = None,
+        forward: Optional[str] = None,
+        sticker_id: Optional[int] = None,
+        group_id: Optional[int] = None,
+        keyboard: Optional[str] = None,
+        payload: Optional[str] = None,
+        dont_parse_links: Optional[bool] = None,
+        disable_mentions: Optional[bool] = None,
+        **kwargs,
+    ) -> int:
+        locals().update(kwargs)
+        locals().pop("kwargs")
+        data = {k: v for k, v in locals().items() if k != "self" and v is not None}
+        required_params = ("peer_id", "user_id", "domain", "chat_id", "user_ids")
+        if not any(data.get(param) for param in required_params):
+            data["peer_id"] = self.peer_id
+        data["forward"] = MessagesForward(
+            conversation_message_ids=[self.conversation_message_id],
+            peer_id=self.peer_id
+        ).json()
+
+        return (await self.ctx_api.request("messages.send", data))["response"]
 
 MessageMin.update_forward_refs()
 
