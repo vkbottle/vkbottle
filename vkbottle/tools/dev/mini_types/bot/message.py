2ae8812467ea5d684170f88ff4057295278a3c47
FeeeeK <krmrclemens@gmail.com>
2021-09-04 22:53:26 +0300

    Some fixes

--- vkbottle/tools/dev/mini_types/bot/message.py
+++ vkbottle/tools/dev/mini_types/bot/message.py
@@ -2,14 +2,14 @@ from typing import Any, List, Optional, Union
 
 from vkbottle_types import StatePeer
 from vkbottle_types.events.bot_events import MessageNew
-from vkbottle_types.objects import MessagesClientInfo, MessagesForward, MessagesMessage, UsersUser
+from vkbottle_types.objects import ClientInfoForBots, MessagesForward, MessagesMessage, UsersUser
 
 from vkbottle.api import ABCAPI, API
 
 
 class MessageMin(MessagesMessage):
     group_id: Optional[int] = None
-    client_info: Optional["MessagesClientInfo"] = None
+    client_info: Optional["ClientInfoForBots"] = None
     unprepared_ctx_api: Optional[Any] = None
     state_peer: Optional[StatePeer] = None
 
@@ -58,24 +58,6 @@ class MessageMin(MessagesMessage):
         self,
         message: Optional[str] = None,
         attachment: Optional[str] = None,
-        user_id: Optional[int] = None,
-        domain: Optional[str] = None,
-        chat_id: Optional[int] = None,
-        random_id: Optional[int] = 0,
-        user_ids: Optional[List[int]] = None,
-        lat: Optional[float] = None,
-        long: Optional[float] = None,
-        reply_to: Optional[int] = None,
-        forward_messages: Optional[List[int]] = None,
-        forward: Optional[str] = None,
-        sticker_id: Optional[int] = None,
-        group_id: Optional[int] = None,
-        keyboard: Optional[str] = None,
-        payload: Optional[str] = None,
-        dont_parse_links: Optional[bool] = None,
-        disable_mentions: Optional[bool] = None,
-        template: Optional[str] = None,
-        intent: Optional[str] = None,
         **kwargs,
     ) -> int:
         locals().update(kwargs)
@@ -85,34 +67,16 @@ class MessageMin(MessagesMessage):
         data["forward"] = MessagesForward(
             conversation_message_ids=[self.conversation_message_id],
             peer_id=self.peer_id,
-            is_reply=True
+            is_reply=True,
         ).json()
 
-        return (await self.ctx_api.request("messages.send", data))["response"]
+        return await self.answer(**data)
 
     async def forward(
         self,
         message: Optional[str] = None,
         attachment: Optional[str] = None,
-        peer_id: Optional[int] = None,
-        user_id: Optional[int] = None,
-        domain: Optional[str] = None,
-        chat_id: Optional[int] = None,
-        random_id: Optional[int] = 0,
-        user_ids: Optional[List[int]] = None,
-        lat: Optional[float] = None,
-        long: Optional[float] = None,
-        reply_to: Optional[int] = None,
-        forward_messages: Optional[List[int]] = None,
-        forward: Optional[str] = None,
-        sticker_id: Optional[int] = None,
-        group_id: Optional[int] = None,
-        keyboard: Optional[str] = None,
-        payload: Optional[str] = None,
-        dont_parse_links: Optional[bool] = None,
-        disable_mentions: Optional[bool] = None,
-        template: Optional[str] = None,
-        intent: Optional[str] = None,
+        forward_message_ids: Optional[List[int]] = None,
         **kwargs,
     ) -> int:
         locals().update(kwargs)
@@ -121,12 +85,15 @@ class MessageMin(MessagesMessage):
         required_params = ("peer_id", "user_id", "domain", "chat_id", "user_ids")
         if not any(data.get(param) for param in required_params):
             data["peer_id"] = self.peer_id
+        if not forward_message_ids:
+            forward_message_ids = [self.conversation_message_id]
+
         data["forward"] = MessagesForward(
-            conversation_message_ids=[self.conversation_message_id],
-            peer_id=self.peer_id
+            conversation_message_ids=forward_message_ids, peer_id=self.peer_id
         ).json()
 
-        return (await self.ctx_api.request("messages.send", data))["response"]
+        return await self.answer(**data)
+
 
 MessageMin.update_forward_refs()
 
