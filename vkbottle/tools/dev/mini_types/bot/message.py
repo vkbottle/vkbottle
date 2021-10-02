8ccc4eaf3f41ff0b054da96f5f5ed3f6e57467e4
FeeeeK <26704473+FeeeeK@users.noreply.github.com>
2021-10-02 22:10:37 +0300

    Update MessageMin

--- vkbottle/tools/dev/mini_types/bot/message.py
+++ vkbottle/tools/dev/mini_types/bot/message.py
@@ -1,82 +1,26 @@
 from io import StringIO
-from typing import TYPE_CHECKING, Any, List, Optional, Union
+from typing import TYPE_CHECKING, List, Optional
 
-from vkbottle_types import StatePeer
 from vkbottle_types.events.bot_events import MessageNew
-from vkbottle_types.objects import ClientInfoForBots, MessagesForward, MessagesMessage, UsersUser
+from vkbottle_types.objects import ClientInfoForBots, MessagesForward
+
+from ..base import BaseMessageMin
 
 if TYPE_CHECKING:
 
     from vkbottle.api import ABCAPI, API
 
 
-class MessageMin(MessagesMessage):
+class MessageMin(BaseMessageMin):
     group_id: Optional[int] = None
     client_info: Optional["ClientInfoForBots"] = None
-    unprepared_ctx_api: Optional[Any] = None
-    state_peer: Optional["StatePeer"] = None
-
-    @property
-    def ctx_api(self) -> Union["ABCAPI", "API"]:
-        return getattr(self, "unprepared_ctx_api")
-
-    async def get_user(self, raw_mode: bool = False, **kwargs) -> Union["UsersUser", dict]:
-        raw_user = (await self.ctx_api.request("users.get", {"user_ids": self.from_id, **kwargs}))[
-            "response"
-        ][0]
-        return raw_user if raw_mode else UsersUser(**raw_user)
-
-    async def answer(
-        self,
-        message: Optional[str] = None,
-        attachment: Optional[str] = None,
-        user_id: Optional[int] = None,
-        random_id: Optional[int] = 0,
-        peer_id: Optional[int] = None,
-        peer_ids: Optional[List[int]] = None,
-        domain: Optional[str] = None,
-        chat_id: Optional[int] = None,
-        user_ids: Optional[List[int]] = None,
-        lat: Optional[float] = None,
-        long: Optional[float] = None,
-        reply_to: Optional[int] = None,
-        forward_messages: Optional[List[int]] = None,
-        forward: Optional[str] = None,
-        sticker_id: Optional[int] = None,
-        group_id: Optional[int] = None,
-        keyboard: Optional[str] = None,
-        template: Optional[str] = None,
-        payload: Optional[str] = None,
-        content_source: Optional[str] = None,
-        dont_parse_links: Optional[bool] = None,
-        disable_mentions: Optional[bool] = None,
-        intent: Optional[str] = None,
-        subscribe_id: Optional[int] = None,
-        **kwargs,
-    ) -> Optional[int]:
-        locals().update(kwargs)
-
-        data = {k: v for k, v in locals().items() if k not in ("self", "kwargs") and v is not None}
-        required_params = ("peer_id", "user_id", "domain", "chat_id", "user_ids")
-        if not any(data.get(param) for param in required_params):
-            data["peer_id"] = self.peer_id
-
-        stream = StringIO(message)
-        response = None
-        while True:
-            data["message"] = stream.read(4096)
-            if not (data["message"] or data.get("attachment")):
-                break
-            response = (await self.ctx_api.request("messages.send", data))["response"]
-            data.pop("attachment", None)
-        return response
 
     async def reply(
         self,
         message: Optional[str] = None,
         attachment: Optional[str] = None,
         **kwargs,
-    ) -> Optional[int]:
+    ) -> int:
         locals().update(kwargs)
 
         data = {k: v for k, v in locals().items() if k not in ("self", "kwargs") and v is not None}
@@ -95,7 +39,7 @@ class MessageMin(MessagesMessage):
         attachment: Optional[str] = None,
         forward_message_ids: Optional[List[int]] = None,
         **kwargs,
-    ) -> Optional[int]:
+    ) -> int:
         locals().update(kwargs)
 
         data = {
@@ -110,9 +54,6 @@ class MessageMin(MessagesMessage):
         ).json()
 
         return await self.answer(**data)
-
-
-MessageMin.update_forward_refs()
 
 
 def message_min(event: dict, ctx_api: "ABCAPI") -> "MessageMin":
--- vkbottle/tools/dev/mini_types/user/message.py
+++ vkbottle/tools/dev/mini_types/user/message.py
@@ -1,79 +1,20 @@
-from io import StringIO
-from typing import TYPE_CHECKING, Any, List, Optional, Union
+from typing import TYPE_CHECKING, List, Optional
 
-from vkbottle_types import StatePeer
-from vkbottle_types.objects import MessagesMessage, UsersUser
+from ..base import BaseMessageMin
 
 if TYPE_CHECKING:
-    from vkbottle.api import ABCAPI, API
+    from vkbottle.api import ABCAPI
 
 
-class MessageMin(MessagesMessage):
+class MessageMin(BaseMessageMin):
     user_id: Optional[int] = None
-    unprepared_ctx_api: Optional[Any] = None
-    state_peer: Optional["StatePeer"] = None
-
-    @property
-    def ctx_api(self) -> Union["ABCAPI", "API"]:
-        return getattr(self, "unprepared_ctx_api")
-
-    async def get_user(self, raw_mode: bool = False, **kwargs) -> Union["UsersUser", dict]:
-        raw_user = (await self.ctx_api.request("users.get", {"user_ids": self.from_id, **kwargs}))[
-            "response"
-        ][0]
-        return raw_user if raw_mode else UsersUser(**raw_user)
-
-    async def answer(
-        self,
-        message: Optional[str] = None,
-        attachment: Optional[str] = None,
-        user_id: Optional[int] = None,
-        random_id: Optional[int] = 0,
-        peer_id: Optional[int] = None,
-        peer_ids: Optional[List[int]] = None,
-        domain: Optional[str] = None,
-        chat_id: Optional[int] = None,
-        user_ids: Optional[List[int]] = None,
-        lat: Optional[float] = None,
-        long: Optional[float] = None,
-        reply_to: Optional[int] = None,
-        forward_messages: Optional[List[int]] = None,
-        forward: Optional[str] = None,
-        sticker_id: Optional[int] = None,
-        group_id: Optional[int] = None,
-        keyboard: Optional[str] = None,
-        template: Optional[str] = None,
-        payload: Optional[str] = None,
-        content_source: Optional[str] = None,
-        dont_parse_links: Optional[bool] = None,
-        disable_mentions: Optional[bool] = None,
-        intent: Optional[str] = None,
-        subscribe_id: Optional[int] = None,
-        **kwargs,
-    ) -> Optional[int]:
-        locals().update(kwargs)
-
-        data = {k: v for k, v in locals().items() if k not in ("self", "kwargs") and v is not None}
-        required_params = ("peer_id", "user_id", "domain", "chat_id", "user_ids")
-        if not any(data.get(param) for param in required_params):
-            data["peer_id"] = self.peer_id
-
-        stream = StringIO(message)
-        response = None
-        while True:
-            data["message"] = stream.read(4096)
-            if not (data["message"] or data.get("attachment")):
-                break
-            response = (await self.ctx_api.request("messages.send", data))["response"]
-            data.pop("attachment", None)
-        return response
 
     async def reply(
         self,
         message: Optional[str] = None,
         attachment: Optional[str] = None,
         **kwargs,
-    ) -> Optional[int]:
+    ) -> int:
         locals().update(kwargs)
 
         data = {k: v for k, v in locals().items() if k not in ("self", "kwargs") and v is not None}
@@ -88,7 +29,7 @@ class MessageMin(MessagesMessage):
         attachment: Optional[str] = None,
         forward_message_ids: Optional[List[int]] = None,
         **kwargs,
-    ) -> Optional[int]:
+    ) -> int:
         locals().update(kwargs)
 
         data = {
