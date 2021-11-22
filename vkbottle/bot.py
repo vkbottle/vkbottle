0a18d4ccc85c7e561ab8f25308934f0d3660d72d
FeeeeK <26704473+FeeeeK@users.noreply.github.com>
2021-11-22 16:26:32 +0300

    Add rules to bot and user

--- vkbottle/bot.py
+++ vkbottle/bot.py
@@ -1,6 +1,17 @@
+from .dispatch.rules import base as rules
 from .dispatch.views import bot as views
 from .framework.bot import ABCBotLabeler, Bot, BotBlueprint, BotLabeler, run_multibot
 from .tools.dev.mini_types.bot import MessageMin
 
 Message = MessageMin
 Blueprint = BotBlueprint
+
+__all__ = (
+    "Bot",
+    "BotBlueprint",
+    "BotLabeler",
+    "ABCBotLabeler",
+    "run_multibot",
+    "rules",
+    "views",
+)
--- vkbottle/user.py
+++ vkbottle/user.py
@@ -1,6 +1,17 @@
+from .dispatch.rules import base as rules
 from .dispatch.views import user as views
 from .framework.user import ABCUserLabeler, User, UserBlueprint, UserLabeler, run_multibot
 from .tools.dev.mini_types.bot import MessageMin
 
 Message = MessageMin
 Blueprint = UserBlueprint
+
+__all__ = (
+    "User",
+    "UserBlueprint",
+    "UserLabeler",
+    "ABCUserLabeler",
+    "run_multibot",
+    "rules",
+    "views",
+)
