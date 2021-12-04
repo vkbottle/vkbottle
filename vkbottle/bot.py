c814837736acd6867c421f1c0e37fd00b67d6f46
FeeeeK <26704473+FeeeeK@users.noreply.github.com>
2021-12-04 21:09:25 +0300

    Fixed error after merge

--- vkbottle/bot.py
+++ vkbottle/bot.py
@@ -2,7 +2,7 @@ from vkbottle.framework.labeler import BotLabeler
 
 from .dispatch.rules import base as rules
 from .dispatch.views import bot as views
-from .framework.bot import ABCBotLabeler, Bot, BotBlueprint, BotLabeler, run_multibot
+from .framework.bot import Bot, BotBlueprint, run_multibot
 from .tools.dev.mini_types.bot import MessageEventMin, MessageMin
 
 Message = MessageMin
