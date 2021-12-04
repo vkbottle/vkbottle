f59d562113883c595b408090703f849962a9eaed
FeeeeK <26704473+FeeeeK@users.noreply.github.com>
2021-12-04 20:59:26 +0300

    Just some fixes

--- vkbottle/tests/conftest.py
+++ vkbottle/tests/conftest.py
@@ -3,3 +3,5 @@ from vkbottle.tests.fixtures.middlewares import (
     empty_middleware_class,
     empty_middleware_instance,
 )
+
+__all__ = ("empty_event", "empty_middleware_class", "empty_middleware_instance")
