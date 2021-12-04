e60d966c7b178c62ec8b390f456a1f39a5e3e6ce
Mironov Danil <71277890+mironovmeow@users.noreply.github.com>
2021-12-04 13:01:41 -0500

    Add supporting of MessageEvent by dataclass (#438)
    * Add MessageEventMin
    
    * update docs
    
    * Some fixes
    
    Co-authored-by: FeeeeK <26704473+FeeeeK@users.noreply.github.com>

--- vkbottle/bot.py
+++ vkbottle/bot.py
@@ -1,9 +1,10 @@
 from .dispatch.rules import base as rules
 from .dispatch.views import bot as views
 from .framework.bot import ABCBotLabeler, Bot, BotBlueprint, BotLabeler, run_multibot
-from .tools.dev.mini_types.bot import MessageMin
+from .tools.dev.mini_types.bot import MessageEventMin, MessageMin
 
 Message = MessageMin
+MessageEvent = MessageEventMin
 Blueprint = BotBlueprint
 
 __all__ = (
@@ -12,6 +13,7 @@ __all__ = (
     "Blueprint",
     "BotLabeler",
     "Message",
+    "MessageEvent",
     "run_multibot",
     "rules",
     "views",
