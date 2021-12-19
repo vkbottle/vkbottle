95a1624405335710f92d415a78547197393664a8
FeeeeK <26704473+FeeeeK@users.noreply.github.com>
2021-12-19 17:05:50 +0300

    Another update for the default request validator

--- vkbottle/api/request_validator/translate_friendly_types_validator.py
+++ vkbottle/api/request_validator/translate_friendly_types_validator.py
@@ -6,12 +6,15 @@ from vkbottle.modules import json
 
 class TranslateFriendlyTypesRequestValidator(ABCRequestValidator):
     async def validate(self, request: dict) -> dict:
-        for k, v in request.items():
-            # translate python-list to vk array-like type
+        for k, v in request.copy().items():
             if isinstance(v, list):
                 request[k] = ",".join(str(e) for e in v)
             elif isinstance(v, bool):
                 request[k] = int(v)
             elif isinstance(v, BaseModel):
                 request[k] = v.json(exclude_none=True, encoder=json.dumps)
+            elif isinstance(v, dict):
+                request[k] = json.dumps(await self.validate(v))
+            elif v is None:
+                del request[k]
         return request
