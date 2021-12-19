00b720c2a09faa45b23c1aad7117da85d7f661de
FeeeeK <26704473+FeeeeK@users.noreply.github.com>
2021-12-19 15:40:58 +0300

    Fix request validators

--- vkbottle/api/request_validator/abc.py
+++ vkbottle/api/request_validator/abc.py
@@ -1,12 +1,12 @@
 from abc import ABC, abstractmethod
-from typing import Any, NoReturn, Union
+from typing import NoReturn, Union
 
 
 class ABCRequestValidator(ABC):
-    """Abstract Response Validator class
-    Documentation: https://github.com/vkbottle/vkbottle/blob/master/docs/low-level/api/response-validator.md
+    """Abstract Request Validator class
+    Documentation: https://github.com/vkbottle/vkbottle/blob/master/docs/low-level/api/request_validator.md
     """
 
     @abstractmethod
-    async def validate(self, response: Any) -> Union[Any, NoReturn]:
+    async def validate(self, request: dict) -> Union[dict, NoReturn]:
         pass
--- vkbottle/api/request_validator/translate_friendly_types_validator.py
+++ vkbottle/api/request_validator/translate_friendly_types_validator.py
@@ -1,10 +1,17 @@
+from pydantic import BaseModel
+
 from .abc import ABCRequestValidator
+from vkbottle.modules import json
 
 
 class TranslateFriendlyTypesRequestValidator(ABCRequestValidator):
-    async def validate(self, response: dict) -> dict:
-        for k, v in response.items():
+    async def validate(self, request: dict) -> dict:
+        for k, v in request.items():
             # translate python-list to vk array-like type
             if isinstance(v, list):
-                response[k] = ",".join(str(e) for e in v)
-        return response
+                request[k] = ",".join(str(e) for e in v)
+            elif isinstance(v, bool):
+                request[k] = int(v)
+            elif isinstance(v, BaseModel):
+                request[k] = v.json(exclude_none=True, encoder=json.dumps)
+        return request
