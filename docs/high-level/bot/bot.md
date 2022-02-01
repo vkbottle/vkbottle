cfc3bb6e03a3793f30346662fae446abab02a3ec
Alexey <30408771+delvinru@users.noreply.github.com>
2022-02-01 12:22:59 +0300

    Callback api (#473)
    * Implemented callback api
    
    * decrease secret_key length
    
    * implement callback functions
    
    * Add function for get secret key from callback
    
    * Add examples for CallbackAPI usage
    
    * Fix typo
    
    * Add documentation for Callback API

--- docs/high-level/bot/bot.md
+++ docs/high-level/bot/bot.md
@@ -8,16 +8,29 @@
 `bot.router` - [Router документация](../../high-level/routing/index.md)
 `bot.labeler`/`bot.on` - [Labeler документация](labeler.md)
 `bot.polling` - [Polling документация](../../low-level/polling/polling.md)
+`bot.callback` - [Callback документация](../../low-level/callback/callback.md)
 `bot.loop_wrapper` - [Loop Wrapper документация](../../tools/loop-wrapper.md)
 `bot.loop` - возвращает _event loop_ который был установлен или самостоятельно получает его
 `bot.error` - возвращает `error handler` бота из `bot.router`. Добавлено для краткой записи
 
 Функции:
 
-## bot.run_polling()
+## Работа через Long Poll API
+
+### bot.run_polling()
 
 Асинхронный запуск longpoll
 
-## bot.run_forever()
+### bot.run_forever()
 
 Синхронный запуск longpoll. Добавляет `run_polling` в таски `bot.loop_wrapper` и запускает луп в `run_forever`
+
+## Работа через Callback API
+
+### bot.setup_webhook()
+
+Устаналивает сервер для `CallbackAPI` в сообщество
+
+### bot.process_event(event)
+
+Передает полученное событие в роутер, для его обработки
---
+++ vkbottle/callback/abc.py
@@ -0,0 +1,57 @@
+from abc import ABC, abstractmethod
+from typing import TYPE_CHECKING, Any, Dict, List, Optional
+
+if TYPE_CHECKING:
+    from vkbottle.api import ABCAPI
+    from vkbottle.exception_factory import ABCErrorHandler
+
+
+class ABCCallback(ABC):
+    """
+    Abstract Callback class
+    """
+
+    @abstractmethod
+    async def add_callback_server(self) -> Any:
+        pass
+
+    @abstractmethod
+    async def delete_callback_server(self, server_id: int) -> Any:
+        pass
+
+    @abstractmethod
+    async def edit_callback_server(self, server_id: int, secret_key: Optional[str] = None) -> Any:
+        pass
+
+    @abstractmethod
+    async def get_callback_confirmation_code(self) -> Any:
+        pass
+
+    @abstractmethod
+    async def get_callback_servers(self, server_ids: Optional[List[int]] = None) -> Any:
+        pass
+
+    @abstractmethod
+    async def get_callback_settings(self, server_id: int) -> Any:
+        pass
+
+    @abstractmethod
+    async def set_callback_settings(
+        self, server_id: int, params: Optional[Dict[str, bool]] = None
+    ) -> Any:
+        pass
+
+    @property
+    @abstractmethod
+    def api(self) -> "ABCAPI":
+        pass
+
+    @api.setter
+    def api(self, new_api: "ABCAPI"):
+        pass
+
+    @abstractmethod
+    def construct(
+        self, api: "ABCAPI", error_handler: Optional["ABCErrorHandler"] = None
+    ) -> "ABCCallback":
+        pass
