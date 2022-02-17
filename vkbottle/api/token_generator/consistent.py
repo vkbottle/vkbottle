b4b6b85a3063282b9b63ec4a792d0855e500c925
deleteduser0206 <deleteduser0206@gmail.com>
2022-02-17 17:40:38 +0200

    Add cycle attribute to ConsistentTokenGenerator

--- vkbottle/api/token_generator/consistent.py
+++ vkbottle/api/token_generator/consistent.py
@@ -6,7 +6,8 @@ from .abc import ABCTokenGenerator
 
 class ConsistentTokenGenerator(ABCTokenGenerator):
     def __init__(self, tokens: Iterable[str]):
-        self.tokens = itertools.cycle(tokens)
+        self.tokens = tokens
+        self.cycle = itertools.cycle(self.tokens)
 
     async def get_token(self) -> str:
-        return next(self.tokens)
+        return next(self.cycle)
