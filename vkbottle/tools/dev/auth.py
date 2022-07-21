d922d3b8a62c0c0af28ba13052d5fe05d57c5258
FeeeeK <26704473+FeeeeK@users.noreply.github.com>
2022-07-21 14:51:58 +0300

    Refactor User Auth flow

--- vkbottle/tools/dev/auth.py
+++ vkbottle/tools/dev/auth.py
@@ -1,19 +1,19 @@
 from typing import Optional
 
+from vkbottle.exception_factory import CaptchaError, VKAPIError
 from vkbottle.http.aiohttp import SingleAiohttpClient
 
 MOBILE_APP_ID = 2274003
 MOBILE_APP_SECRET = "hHbZxrka2uZ6jB1inYsH"
 
 
-class AuthError(Exception):
-    def __init__(self, typ: str, description: str):
-        super().__init__(description)
-        self.type = typ
-        self.description = description
+class AuthError(VKAPIError):
+    pass
 
 
 class UserAuth:
+    AUTH_URL = "https://oauth.vk.com/token"
+
     def __init__(self, client_id: Optional[int] = None, client_secret: Optional[str] = None):
         if client_id is not None and client_secret is not None:
             self.client_id = client_id
@@ -22,19 +22,28 @@ class UserAuth:
             self.client_id = MOBILE_APP_ID
             self.client_secret = MOBILE_APP_SECRET
 
-    def _build_oauth_url(self, login: str, password: str) -> str:
-        return (
-            f"https://oauth.vk.com/token?grant_type=password&client_id={self.client_id}"
-            f"&client_secret={self.client_secret}&username={login}&password={password}"
-        )
+    def _get_params(self, login: str, password: str) -> dict:
+        return {
+            "grant_type": "password",
+            "client_id": self.client_id,
+            "client_secret": self.client_secret,
+            "username": login,
+            "password": password,
+        }
 
     async def get_token(self, login: str, password: str) -> str:
-        url = self._build_oauth_url(login, password)
+        params = self._get_params(login, password)
 
         client = SingleAiohttpClient()
-        response = await client.request_json(url)
+        response = await client.request_json(
+            url=self.AUTH_URL,
+            method="POST",
+            data=params,
+        )
 
         if "access_token" in response:
             return response["access_token"]
-
-        raise AuthError(response["error"], response["error_description"])
+        response["error_msg"] = response.pop("error")
+        if response["error_msg"] == "need_captcha":
+            raise CaptchaError(**response, request_params=[])
+        raise AuthError(**response, request_params=[])
