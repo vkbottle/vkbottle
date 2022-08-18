ac696a0053c7ea380eb8a7cf4a77ce7a1ed27a3e
FeeeeK <26704473+FeeeeK@users.noreply.github.com>
2022-08-18 06:41:45 +0300

    Update template generator (fix #536)

--- vkbottle/tools/dev/template/element.py
+++ vkbottle/tools/dev/template/element.py
@@ -1,6 +1,8 @@
 from typing import List, Optional, Union
 
 from vkbottle.modules import json
+from vkbottle.tools.dev.keyboard import Keyboard
+from vkbottle.tools.dev.keyboard.button import KeyboardButton
 
 
 class TemplateElement:
@@ -11,15 +13,21 @@ class TemplateElement:
         title: Optional[str] = None,
         description: Optional[str] = None,
         photo_id: Optional[str] = None,
-        buttons: Optional[Union[List[dict], str]] = None,
+        buttons: Optional[Union[List[KeyboardButton], List[dict], str]] = None,
         action: Optional[dict] = None,
     ):
         assert buttons, "Buttons are required"
         assert photo_id or title, "photo_id or title is required"
 
         if isinstance(buttons, str):
-            buttons = json.loads(buttons)  # type: ignore
+            buttons = json.loads(buttons)["buttons"][0]  # type: ignore
 
         if isinstance(buttons, dict):
             buttons = buttons["buttons"][0]  # type: ignore
+
+        if isinstance(buttons, Keyboard):
+            buttons = [button.get_data() for button in buttons.buttons[0]]
+
+        if all(isinstance(button, KeyboardButton) for button in buttons):  # type: ignore
+            buttons = [button.get_data() for button in buttons]  # type: ignore
         self.raw: dict = {k: v for k, v in locals().items() if v is not None and k != "self"}
