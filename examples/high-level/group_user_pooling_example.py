959c6b1bf581a6fcda7123f2dfadeca35d6bc23e
FeeeeK <26704473+FeeeeK@users.noreply.github.com>
2022-10-09 16:48:43 +0300

    Fix group user polling example

--- examples/high-level/group_user_pooling_example.py
+++ examples/high-level/group_user_pooling_example.py
@@ -1,6 +1,7 @@
 from typing import TYPE_CHECKING, Optional
 
 from vkbottle_types.events.enums import UserEventType
+from vkbottle_types.events.user_events import RawUserEvent
 
 from vkbottle import Bot
 from vkbottle.framework.labeler import UserLabeler
@@ -20,13 +21,21 @@ class BotMessagesPooling(UserPolling):
     def __init__(
         self,
         api: Optional["ABCAPI"] = None,
-        group_id: Optional[int] = None,
+        user_id: Optional[int] = None,
         wait: Optional[int] = None,
         mode: Optional[int] = None,
         rps_delay: Optional[int] = None,
         error_handler: Optional["ABCErrorHandler"] = None,
+        group_id: Optional[int] = None,
     ):
-        super().__init__(api, wait, mode, rps_delay, error_handler)
+        super().__init__(
+            api=api,
+            user_id=user_id,
+            wait=wait,
+            mode=mode,
+            rps_delay=rps_delay,
+            error_handler=error_handler,
+        )
         self.group_id = group_id
 
     async def get_server(self) -> dict:
@@ -40,15 +49,11 @@ token = "..."
 bot = Bot(token, labeler=UserLabeler(), polling=BotMessagesPooling())
 
 
-@bot.on.raw_event(UserEventType.CHAT_INFO_EDIT)
+@bot.on.raw_event(UserEventType.CHAT_INFO_EDIT, dataclass=RawUserEvent)  # type: ignore
 async def process_event(event):
-    if event.object[1] == 7:
-        type_action = "left"
-    elif event.object[1] == 6:
-        type_action = "returned to the"
+    type_action = "left" if event.object[1] == 7 else "returned to the"
     if event.object[1] in (6, 7):
         logger.info("User {} {} conversation {}.", event.object[3], type_action, event.object[2])
 
 
-if __name__ == "__main__":
-    bot.run_forever()
+bot.run_forever()
