e10f7ee54c53b4635a18cbab510c42cf7e4cd9ca
Georgiy <{ID}+{username}@users.noreply.github.com>
2022-10-12 15:12:54 +0300

    fix TypeError: missing request_params argument

--- vkbottle/exception_factory/base_exceptions.py
+++ vkbottle/exception_factory/base_exceptions.py
@@ -1,12 +1,16 @@
-from typing import List
+from typing import List, Optional
 
 from .code_exception import CodeException
 
 
 class VKAPIError(CodeException):
-    def __init__(self, *, error_msg: str, request_params: List[dict]):
+    def __init__(self, *, error_msg: str, request_params: Optional[List[dict]] = None):
         self.description = error_msg
-        self.params = {item["key"]: item["value"] for item in request_params}
+        # server maybe return error code without request_params key
+        if request_params:
+            self.params = {item["key"]: item["value"] for item in request_params}
+        else:
+            self.params = {}
 
     def __str__(self) -> str:
         return self.description
