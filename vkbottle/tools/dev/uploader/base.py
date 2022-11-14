0bbb64a017e979abccc558e3b5600ccffc6fdcd6
FeeeeK <26704473+FeeeeK@users.noreply.github.com>
2022-11-14 23:23:11 +0300

    Fix E303 error

--- vkbottle/tools/dev/uploader/base.py
+++ vkbottle/tools/dev/uploader/base.py
@@ -46,7 +46,6 @@ class BaseUploader(ABC):
         )
         return json.loads(raw_response)
 
-
     def get_bytes_io(self, data: "Bytes", name: Optional[str] = None) -> BytesIO:
         bytes_io = data if isinstance(data, BytesIO) else BytesIO(data)
         # To avoid errors with image generators (such as pillow)
