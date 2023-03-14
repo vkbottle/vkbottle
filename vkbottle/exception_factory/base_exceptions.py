9334a6edc34325f76d362280aa870c32311055b0
FeeeeK <26704473+FeeeeK@users.noreply.github.com>
2023-03-14 12:20:44 +0300

    Add fallback for API errors with unexpected kwargs

--- vkbottle/exception_factory/base_exceptions.py
+++ vkbottle/exception_factory/base_exceptions.py
@@ -2,26 +2,40 @@ from typing import List, Optional
 
 from .code_exception import CodeException
 from .reducible_kwargs_exception import ReducibleKwargsException
+from vkbottle.modules import logger
 
 
 class VKAPIError(CodeException, ReducibleKwargsException):
-    def __init__(self, *, error_msg: str, request_params: Optional[List[dict]] = None):
+    def __init__(self, *, error_msg: str, request_params: Optional[List[dict]] = None, **kwargs):
         self.error_msg = error_msg
         # server maybe return error code without request_params key
         if request_params:
             self.request_params = {item["key"]: item["value"] for item in request_params}
         else:
             self.request_params = {}
+        if kwargs:
+            logger.warning("VK API Error {} has extra kwargs: {}", self.code, kwargs)
+        self.kwargs = kwargs
 
     def __str__(self) -> str:
         return self.error_msg
 
 
 class CaptchaError(VKAPIError, code=14):
-    def __init__(self, *, captcha_sid: str, captcha_img: str, **kwargs):
+    def __init__(
+        self,
+        *,
+        captcha_sid: str,
+        captcha_img: str,
+        captcha_ts: Optional[str] = None,
+        captcha_attempt: Optional[int] = None,
+        **kwargs,
+    ):
         super().__init__(**kwargs)
         self.captcha_sid = int(captcha_sid)
         self.captcha_img = captcha_img
+        self.captcha_ts = captcha_ts
+        self.captcha_attempt = captcha_attempt
 
 
 class APIAuthError(VKAPIError, code=5):
