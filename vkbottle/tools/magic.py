99cb75218971a689718887d789b3b954bf15fe65
luwqz1 <howluwqz1@gmail.com>
2025-06-14 20:52:56 +0300

    refactor: improve magic_bundle

--- vkbottle/tools/magic.py
+++ vkbottle/tools/magic.py
@@ -1,10 +1,66 @@
+from __future__ import annotations
+
 import typing
 
 Function = typing.Callable[..., typing.Any]
 
 
-def resolve_arg_names(func: Function, start_idx: int = 1) -> typing.Tuple[str, ...]:
-    return func.__code__.co_varnames[start_idx : func.__code__.co_argcount]
+def _resolve_arg_names(
+    func: Function,
+    /,
+    *,
+    start_idx: int,
+    stop_idx: int,
+    exclude: set[str] | None = None,
+) -> tuple[str, ...]:
+    exclude = exclude or set()
+    varnames = func.__code__.co_varnames[start_idx:stop_idx]
+    return tuple(name for name in varnames if name not in exclude)
+
+
+def resolve_arg_names(
+    func: Function,
+    /,
+    *,
+    start_idx: int = 1,
+    exclude: set[str] | None = None,
+) -> tuple[str, ...]:
+    return _resolve_arg_names(
+        func,
+        start_idx=start_idx,
+        stop_idx=func.__code__.co_argcount + func.__code__.co_kwonlyargcount,
+        exclude=exclude,
+    )
+
+
+def resolve_kwonly_arg_names(
+    func: Function,
+    /,
+    *,
+    start_idx: int = 1,
+    exclude: set[str] | None = None,
+) -> tuple[str, ...]:
+    return _resolve_arg_names(
+        func,
+        start_idx=func.__code__.co_argcount + start_idx,
+        stop_idx=func.__code__.co_argcount + func.__code__.co_kwonlyargcount,
+        exclude=exclude,
+    )
+
+
+def resolve_posonly_arg_names(
+    func: Function,
+    /,
+    *,
+    start_idx: int = 1,
+    exclude: set[str] | None = None,
+) -> tuple[str, ...]:
+    return _resolve_arg_names(
+        func,
+        start_idx=start_idx,
+        stop_idx=func.__code__.co_posonlyargcount,
+        exclude=exclude,
+    )
 
 
 def get_default_args(func: Function, /) -> typing.Dict[str, typing.Any]:
@@ -26,14 +82,21 @@ def magic_bundle(
     kwargs: typing.Dict[str, typing.Any],
     *,
     start_idx: int = 1,
+    exclude: set[str] | None = None,
 ) -> typing.Dict[str, typing.Any]:
     """This function returns a dictionary containing all default arguments of the function,
     merged with the provided keyword arguments.
     """
-    arg_names = resolve_arg_names(func, start_idx)
+    arg_names = resolve_arg_names(func, start_idx=start_idx, exclude=exclude)
     default_args = get_default_args(func)
     default_args.update({k: v for k, v in kwargs.items() if k in arg_names})
     return default_args
 
 
-__all__ = ("resolve_arg_names", "get_default_args", "magic_bundle")
+__all__ = (
+    "magic_bundle",
+    "get_default_args",
+    "resolve_arg_names",
+    "resolve_kwonly_arg_names",
+    "resolve_posonly_arg_names",
+)
